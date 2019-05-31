93b415b330d4316b6209e9ec3339e201d15afff6
Joseph Cadag <jci.cadag@gmail,com>
2019-05-31 18:18:20 +0800

    Update ExampleUnitTest.java

--- Joseph/ShowMessage/messageviewer/src/test/java/com/example/messageviewer/ExampleUnitTest.java
+++ Joseph/ShowMessage/messageviewer/src/test/java/com/example/messageviewer/ExampleUnitTest.java
@@ -2,18 +2,18 @@ package com.example.messageviewer;
 
 import android.content.Context;
 import org.junit.Test;
-import static org.junit.Assert.*;
+        import static org.junit.Assert.*;
 
 
 public class ExampleUnitTest {
 
-    Context mockContext;
-
     @Test
-    public void testShowToast(){
-        Message message = new Message(mockContext);
-        message.ShowMessage(true);
-        assertEquals("Something wrong, Try again!", message.getMessage());
+    public void testNullContext(){
+        try {
+            Message message = new Message(null);
+        }
+        catch (Exception e){
+            assertEquals("context == null", e.getMessage());
+        }
     }
-
 }
