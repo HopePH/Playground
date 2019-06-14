d0665d8259b72e7fd61e58b9ebcbe8604919acab
Joseph Cadag <jci.cadag@gmail,com>
2019-06-14 16:43:51 +0800

    Commit
    commit calculator prism

---
+++ Joseph/Calculator Prism/Calculator Prism/Calculator Prism/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System.Windows.Input;
+
+namespace Calculator_Prism.ViewModels
+{
+	public class MainPageViewModel : BindableBase
+	{
+		public ICommand AddCommand => new DelegateCommand(Add);
+
+		public string Title { get; set; }
+		public string FirstNumber { get; set; }
+		public string SecondNumber { get; set; }
+		public string Total { get; set; }
+
+		public void Add()
+		{
+			int Sum = int.Parse(FirstNumber) + int.Parse(SecondNumber);
+			Total = Sum.ToString();
+		}
+	}
+}
