6575ac30379efc4445858aa33edc10b2163fe672
Alfie Salano <alfiesalano.pangga@gmail.com>
2019-07-05 22:22:42 +0800

    i

--- Xamarin.WorkShop/BasicMVVM/BasicMVVM/MainPage.xaml.cs
+++ Xamarin.WorkShop/BasicMVVM/BasicMVVM/MainPage.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 using Xamarin.Forms;
 
 namespace BasicMVVM
@@ -16,6 +11,7 @@ namespace BasicMVVM
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = new MainPageViewModel();
         }
     }
 }
--- Xamarin.WorkShop/BasicMVVM/BasicMVVM/MainPageViewModel.cs
+++ Xamarin.WorkShop/BasicMVVM/BasicMVVM/MainPageViewModel.cs
@@ -1,10 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace BasicMVVM
 {
-    class MainPageViewModel
+    public class MainPageViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand MathCommand => new Command(GenerateRandomNo);
+        public string Result { get; set; } = "";
+
+        public MainPageViewModel()
+        {
+        }
+
+        private void GenerateRandomNo()
+        {
+            int resultDouble = RandomNumber(1, 1000000);
+            Result = $"My generated no: {resultDouble.ToString()}";
+            NotifyPropertyChanged(nameof(Result));
+        }
+
+        public int RandomNumber(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max);
+        }
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
