29791541afd4b80ae10dd5f56db69a26600fee53
Joseph Cadag <jci.cadag@gmail.com>
2019-07-23 17:53:56 +0800

    Commit
    presentation for july 24, 2019

---
+++ Joseph/SimpleMessage/SimpleMessage/SimpleMessage/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using SimpleMessage.Utility;
+using System;
+using Xamarin.Forms;
+
+namespace SimpleMessage
+{
+	public partial class MainPage : ContentPage
+	{
+		public object CrossCurrentActivity { get; set; }
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		protected override void OnAppearing()
+		{
+			BtnClicked.Clicked += BtnClicked_Clicked;
+		}
+
+		private void BtnClicked_Clicked(object sender, EventArgs e)
+		{
+			DependencyService.Get<IShowUtility>().ShowMessage();
+		}
+	}
+}
