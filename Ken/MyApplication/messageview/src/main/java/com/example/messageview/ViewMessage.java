b45344fbb7b2247d58377784db84936e762d9cf7
mckenley <53161032+mckenley@users.noreply.github.com>
2019-07-25 14:13:18 +0800

    My Projects
    My Projects

---
+++ Ken/MyApplication/messageview/src/main/java/com/example/messageview/ViewMessage.java
@@ -0,0 +1,63 @@
+package com.example.messageview;
+
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+
+import static androidx.core.util.Preconditions.checkNotNull;
+
+public class ViewMessage {
+    private final Context _context;
+    private String _message;
+
+    @SuppressLint("RestrictedApi")
+    public ViewMessage(@NonNull Context context) {
+        checkNotNull(context, "Success!");
+        this._context = context;
+    }
+
+    public void MessageView(boolean isToast) {
+        try {
+            if (this._context != null) {
+                if (isToast)
+                    try {
+                        Toast.makeText(this._context, "Hello World!", Toast.LENGTH_SHORT).show();
+                    }
+                    catch (RuntimeException re){
+                        if (!re.getMessage().contains("not mocked")) throw re;
+                    }
+                else {
+                    try {
+                        AlertDialog alertDialog = new AlertDialog.Builder(this._context).create();
+                        alertDialog.setTitle("Message Alert");
+                        alertDialog.setMessage("Thank you Message!");
+                        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                    }
+                                });
+                        alertDialog.show();
+                    }
+                    catch (RuntimeException re){
+                        if (!re.getMessage().contains("not mocked")) throw re;
+                    }
+                }
+            }
+
+            this._message = "Message success!";
+        } catch (Exception e) {
+            this._message = e.getMessage();
+        }
+    }
+
+    public String getMessage() {
+        return this._message;
+    }
+}
