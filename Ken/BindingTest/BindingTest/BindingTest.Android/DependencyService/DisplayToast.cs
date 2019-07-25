9d0d039bee16696ad74bbb65046f4d17f1687192
mckenley <53161032+mckenley@users.noreply.github.com>
2019-07-25 14:51:20 +0800

    Moved my project from 'repos'.
    Moved my project from 'repos'.

---
+++ Ken/BindingTest/BindingTest/BindingTest.Android/DependencyService/DisplayToast.cs
@@ -0,0 +1,29 @@
+﻿
+using BindingTest.Utility;
+using Xamarin.Forms;
+using Plugin.CurrentActivity;
+using System;
+
+using Com.Example.Message;
+
+[assembly: Dependency(typeof(BindingTest.Droid.DependencyService.DisplayToast))]
+namespace BindingTest.Droid.DependencyService
+{
+    public class DisplayToast : IDisplayToast
+    {
+        private bool _isToast = true;
+        public void DisplayToastMessage()
+        {
+            try
+            {
+                var activity = CrossCurrentActivity.Current.Activity;
+                ViewMessage viewMessage = new ViewMessage(activity);
+                viewMessage.MessageView(_isToast);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
---
+++ Ken/BindingTest/BindingTest/BindingTest/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using BindingTest.Utility;
+using System;
+using Xamarin.Forms;
+
+namespace BindingTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public object CrossCurrentActivity { get; private set; }
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            Btn_displayToast.Clicked += Button_Clicked;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DependencyService.Get<IDisplayToast>().DisplayToastMessage();
+        }
+    }
+}
