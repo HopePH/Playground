8cec517b05ba159ddc4aba98ecf8cdbe4de261b1
joseph cadag <jci.cadag@gmail.com>
2019-08-01 00:56:49 +0800

    Commit
    iOSDesign

---
+++ Joseph/iOSDesign/iOSDesign/Views/Effects/SearchBarEffect.cs
@@ -0,0 +1,11 @@
+﻿using Xamarin.Forms;
+
+namespace iOSDesign.Views
+{
+    public class SearchBarEffect : RoutingEffect
+    {
+        public SearchBarEffect() : base("iOSDesign.SearchBarEffect")
+        {
+        }
+    }
+}
