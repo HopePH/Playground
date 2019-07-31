6950baf6afdb7424561c6970498bb77ef6383014
mckenley <53161032+mckenley@users.noreply.github.com>
2019-07-31 18:49:05 +0800

    SpecFlow Practice

---
+++ Ken/SpecFlowPractice/SpecFlowPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using TechTalk.SpecFlow;
+namespace SpecFlowPractice
+{
+    class Program
+    {
+        static void Main1(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Ken/SpecFlowPractice/SpecFlowPractice/Steps/SpecFlowFeatureSteps.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowPractice
+{
+    [Binding]
+    class SpecFlowFeatureSteps
+    {
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int numbers)
+        {
+            Console.WriteLine(numbers);
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            Console.WriteLine("I pressed add.");
+        }
+
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int result)
+        {
+            if (result == 120)
+            {
+                Console.WriteLine("The test PASSED");
+            }
+            else
+            {
+                Console.WriteLine("The test FAILED");
+                throw new Exception("The value is different!");
+            }
+                
+        }
+
+    }
+}
