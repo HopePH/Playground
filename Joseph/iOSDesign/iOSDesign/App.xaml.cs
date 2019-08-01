6f21d0726e3596580bfba49480d2afb459299629
Joseph Cadag <jci.cadag@gmail.com>
2019-08-01 12:21:23 +0800

    Commit2
    commit

--- Joseph/iOSDesign/iOSDesign/App.xaml.cs
+++ Joseph/iOSDesign/iOSDesign/App.xaml.cs
@@ -1,16 +1,17 @@
-﻿using System;
+﻿using Prism;
+using Prism.Ioc;
+using Prism.Unity;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace iOSDesign
 {
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
-        public App()
+        public App(IPlatformInitializer initializer = null) : base(initializer)
         {
-            InitializeComponent();
-
-            MainPage = new MainPage();
+            
         }
 
         protected override void OnStart()
@@ -26,6 +27,18 @@ namespace iOSDesign
         protected override void OnResume()
         {
             // Handle when your app resumes
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<MainPage>();
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+        }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+            NavigationService.NavigateAsync("NavigationPage/MainPage");
         }
     }
 }
--- Joseph/iOSDesign/iOSDesign/Views/MainPage.xaml.cs
+++ Joseph/iOSDesign/iOSDesign/Views/MainPage.xaml.cs
@@ -1,21 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System;
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace iOSDesign
 {
-    // Learn more about making custom code visible in the Xamarin.Forms previewer
-    // by visiting https://aka.ms/xamarinforms-previewer
-    [DesignTimeVisible(false)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
         }
     }
 }
